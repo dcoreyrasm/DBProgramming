a3a96583c778f0cd65f0c4f6183beac41a786034
dcoreyrasm <dcorey.rasmussen@gmail.com>
2018-01-09 19:17:39 -0500

    removed unneeded javascript
    removed unneeded javascript

--- module2/mod2_deleterecord.php
+++ module2/mod2_deleterecord.php
@@ -1,22 +1,18 @@
 <html>
-		<head>
-	<title>Find a record</title>
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="styles/custom.css" />
-<link rel="stylesheet" href="themes/rasmussenthemeroller.min.css" />
-<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
-<link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" />
-<script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
-<script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script>
-<script src="javascript/storage.js"></script>
+<head>
+  <title>My First PHP Page</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+  <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
 </head>
 	<body>
-		<div id="page" data-role="page" data-theme="b" >
-	<div data-role="header" data-theme="b">
+
 <h1>
 	Find a record
-		</h1>	</div>
-				<div data-role="content">
+		</h1>
 	<?php
 	include 'mod2_config_sampledb.php';
 	include 'mod2_opendb.php';
@@ -55,8 +51,6 @@ mysqli_close($conn);
 
 
 
-				<div data-role="footer" data-theme="b">
 	  <h4>YOUR APP NAME &copy; 2016</h4>
-	</div>
 	</body>
 </html>
